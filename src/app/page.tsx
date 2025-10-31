827257fa8f0919b877807fc8226973a8567f0474
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-10-31 13:36:49 +0530

    feat:scdcn setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+"use client"
+import { Button } from '@/components/ui/button'
+import { Toaster,toast } from 'sonner'
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className='flex justify-center items-center flex-col min-h-screen bg-black text-white'>
+      <Toaster position='top-center' richColors />
+      <div className='text-6xl  font-bold animate__animated animate__bounce text-center'>
+  Coming Soon!
+      </div>
+      <Button className='mt-12' onClick={()=>{
+        toast.success("we are launching soon!");
+      }}>Click me</Button>
+    </div>
   )
 }
 
